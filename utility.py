0e5e8a7e835c03855bcc8e9864031bcc74454e59
shuklasid19 <shuklasid19@gmail.com>
2022-08-12 11:02:47 +0530

    changes done

--- utility.py
+++ utility.py
@@ -1,28 +1,26 @@
 from pyspark.sql import SparkSession
 
-spark = SparkSession.builder.master('local[2]').appName('MySparkPractise').getOrCreate()
+spark = SparkSession.builder.master('local[2]').appName('text').getOrCreate()
 print('Sparksession:',spark)
 
-"""
-Reading text file to dataframe
-"""
-sample_text= spark.read.format('csv').options(header='True', inferSchema='True', delimiter= '\t') \
-                  .load('sample3.txt')
+#using tab as delimiter and reading the text file with given parameters
+sample_text= spark.read.format('csv').options(header='True', inferSchema='True', delimiter= '\t').load('sample3.txt')
 
 
+#it will show all text in non truncated way
 sample_text.show(truncate=False)
 
+#print the schema of text file
 sample_text.printSchema()
 
-print('Shape of df:', sample_text.count(), ' * ', len(sample_text.columns))
+#the number of rows and columns
+print('Rows:', sample_text.count(), ' columns ', len(sample_text.columns))
 
-"""
-writing dataframe to text file 
-"""
 
-print("Writing tab'\t'")
-sample_text.write.mode('overwrite').csv(path='./Output/user_Text', header='True', sep= '\t')
+
+print("Writinig file in given folder and saving as '")
+sample_text.write.mode('overwrite').csv(path='./Output/user_text_overwritten', header='True', sep= '\t')
 print("Writing | seperated txt File")
-sample_text.write.mode('overwrite').csv(path='./Output/user_Text1', header='True', sep= '|')
+sample_text.write.mode('overwrite').csv(path='./Output/user_text1_overwritten', header='True', sep= '|')
 
 
